c272a52745a97a9cf66f7c7a7117b9276f449323
Matheus <matheussts18@hotmail.com>
2026-04-16 17:00:56 -0300

    revelando a pastaa

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,8 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
 
 from flask import Flask
 from extensions import db, lm, limiter 
