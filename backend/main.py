a8458ca11692a32f205104b89f2776f9201ddb65
Matheus <matheussts18@hotmail.com>
2026-04-16 23:18:25 -0300

    mais debug

--- backend/main.py
+++ backend/main.py
@@ -18,6 +18,7 @@ app = Flask(__name__,
 database_url = os.getenv('DATABASE_URL')
 
 if database_url:
+    print("EXISTE O DATABASE ")
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
