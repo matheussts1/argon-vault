d17f3140762fcb14396c790f27609b084988e025
Matheus <matheussts18@hotmail.com>
2026-04-17 00:35:09 -0300

    removing prints

--- backend/main.py
+++ backend/main.py
@@ -7,19 +7,16 @@ sys.path.append(BASE_DIR)
 from flask import Flask
 from extensions import db, lm, limiter 
 
-print("DEBUG 1: Iniciando script", file=sys.stderr)
 app = Flask(__name__, 
             template_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates')),
             static_folder=os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'static')))
 
 database_url = os.getenv('DATABASE_URL')
-print(f"Database URL: {database_url}")
 
 if database_url:
     if database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
-    print("DEBUG 3: Configurado para POSTGRES", file=sys.stderr)
 else:
     bdbase = os.path.abspath(os.path.dirname(__file__))
     instance_path = os.path.join(bdbase, 'instance')
@@ -27,7 +24,6 @@ else:
         os.makedirs(instance_path)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(instance_path, 'user.db')
-    print("DEBUG 3: Configurado para SQLITE", file=sys.stderr)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -35,15 +31,11 @@ from routes import main_bp
 
 try:
     db.init_app(app)
-    print("DEBUG 4: db.init_app concluído", file=sys.stderr)
     
     with app.app_context():
-        print(f"DEBUG 5: URI dentro do contexto: {app.config['SQLALCHEMY_DATABASE_URI'][:15]}...", file=sys.stderr)
         db.create_all()
-        print("DEBUG 6: db.create_all finalizado", file=sys.stderr)
 except Exception as e:
     import traceback
-    print("DEBUG ERRO: Falha ao inicializar o banco de dados: ", {e})
     traceback.print_exc()
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
