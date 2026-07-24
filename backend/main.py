1e506f393c6500f133b226a7f2c8b1eb30be5490
Matheus <matheussts18@hotmail.com>
2026-07-23 21:44:54 -0300

    refactor: organizing

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ MAIN_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(MAIN_FILE_DIR)
 
 from flask import Flask
+from routes import main_bp
 from extensions import data_base, login_manager, limiter
 from data_base_config import database_path, connection_to_url, local_db_connection
 
@@ -17,9 +18,6 @@ if database_path:
    app.config['SQLALCHEMY_DATABASE_URI'] = connection_to_url(database_path)
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = local_db_connection()
-
-from routes import main_bp
-app.register_blueprint(main_bp)
 
 try:
     login_manager.init_app(app)
@@ -33,6 +31,7 @@ except Exception:
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 login_manager.login_view = 'main.auth_login'
+app.register_blueprint(main_bp)
 
 from routes import *
 
