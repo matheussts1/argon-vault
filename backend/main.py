9e58359e061da9903e5376ec25c4c3c05bb10d85
Matheus <matheussts18@hotmail.com>
2026-07-23 21:41:28 -0300

    refactor: tests

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,8 @@ if database_path:
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = local_db_connection()
 
-from routes import main_bp 
+from routes import main_bp
+app.register_blueprint(main_bp)
 
 try:
     login_manager.init_app(app)
@@ -27,13 +28,11 @@ try:
     
     with app.app_context():
         data_base.create_all()
-except Exception as e:
+except Exception:
     traceback.print_exc()
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
 login_manager.login_view = 'main.auth_login'
-app.register_blueprint(main_bp)
 
 from routes import *
 
