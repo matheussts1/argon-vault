0759d3cbfa5b860e3011de13100b62b7c72941c9
Matheus <matheussts18@hotmail.com>
2026-07-23 21:21:46 -0300

    refactor: creating one file just for the configs

--- backend/main.py
+++ backend/main.py
@@ -28,15 +28,6 @@ try:
 except Exception as e:
     traceback.print_exc()
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-app.config.update(
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax',
-)
-
 login_manager.init_app(app)
 limiter.init_app(app)
 login_manager.login_view = 'main.auth_login'
