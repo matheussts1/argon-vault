438716093ce53d97dd982e466a00f35787e174fc
Matheus <matheussts18@hotmail.com>
2026-07-23 21:24:34 -0300

    fix: rollback

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,3 @@ import os
 
 from main import app
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-app.config.update(
-    SESSION_COOKIE_SECURE=True,
-    SESSION_COOKIE_HTTPONLY=True,
-    SESSION_COOKIE_SAMESITE='Lax',
-)
